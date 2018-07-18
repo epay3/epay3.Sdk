440ef92af808756cc6d28ff03ca5f58fe6f7c86e
Brennan McElhanon <brennan@epay3.com>
2018-07-18 15:17:22 -0500

    Added unit test for the new payerfee endpoint.

---
+++ epay3.Web.Api.Sdk/Model/PayerFeeRequestModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epay3.Web.Api.Sdk.Model
+{
+    [DataContract]
+    public class PayerFeeRequestModel
+    {
+        /// <summary>
+        /// The amount from which to calculate the payer fee.
+        /// </summary>
+        [DataMember(Name = "id", EmitDefaultValue = false)]
+        public decimal Amount { get; set; }
+
+        /// <summary>
+        /// Returns the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
---
+++ epay3.Web.Api.Sdk/Model/PayerFeeResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace epay3.Web.Api.Sdk.Model
+{
+    public class PayerFeeResponseModel
+    {
+        public decimal AchFee { get; set; }
+        public decimal CreditCardFee { get; set; }
+    }
+}
