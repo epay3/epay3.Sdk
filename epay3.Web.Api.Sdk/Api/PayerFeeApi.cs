0ab5c8381cc8e317353f243169433acdd3902df0
Brennan McElhanon <brennan@epay3.com>
2018-07-18 15:17:22 -0500

    Added unit test for the new payerfee endpoint.

---
+++ epay3.Web.Api.Sdk/Api/PayerFeeApi.cs
@@ -0,0 +1,178 @@
+﻿using epay3.Web.Api.Sdk.Client;
+using epay3.Web.Api.Sdk.Model;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epay3.Web.Api.Sdk.Api
+{
+    public interface IPayerFeeApi
+    {
+        /// <summary>
+        /// Creates a temporary "session" with parameters so that the user can be forwarded to the payment page with this context.
+        /// </summary>
+        /// <param name="payerFeeRequestModel">Contains the parameters for payer fee request.</param>
+        /// <param name="impersonationAccountKey">The key that allows impersonation of another account for which the transaction is being processed. Only specify a value if the account being impersonated is different from the account that is submitting this request.</param>
+        PayerFeeResponseModel PayerFeePost(PayerFeeRequestModel payerFeeRequestModel, string impersonationAccountKey);
+    }
+
+    /// <summary>
+    /// Represents a collection of functions to interact with the API endpoints
+    /// </summary>
+    public class PayerFeeApi : IPayerFeeApi
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PayerFeeApi"/> class.
+        /// </summary>
+        /// <returns></returns>
+        public PayerFeeApi(String basePath)
+        {
+            this.Configuration = new Configuration(new ApiClient(basePath));
+
+            // ensure API client has configuration ready
+            if (Configuration.ApiClient.Configuration == null)
+            {
+                this.Configuration.ApiClient.Configuration = this.Configuration;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PaymentPageSessionsApi"/> class
+        /// using Configuration object
+        /// </summary>
+        /// <param name="configuration">An instance of Configuration</param>
+        /// <returns></returns>
+        public PayerFeeApi(Configuration configuration = null)
+        {
+            if (configuration == null) // use the default one in Configuration
+                this.Configuration = Configuration.Default;
+            else
+                this.Configuration = configuration;
+
+            // ensure API client has configuration ready
+            if (Configuration.ApiClient.Configuration == null)
+            {
+                this.Configuration.ApiClient.Configuration = this.Configuration;
+            }
+        }
+
+        /// <summary>
+        /// Gets the base path of the API client.
+        /// </summary>
+        /// <value>The base path</value>
+        public String GetBasePath()
+        {
+            return this.Configuration.ApiClient.RestClient.BaseUrl.ToString();
+        }
+
+        /// <summary>
+        /// Sets the base path of the API client.
+        /// </summary>
+        /// <value>The base path</value>
+        [Obsolete("SetBasePath is deprecated, please do 'Configuraiton.ApiClient = new ApiClient(\"http://new-path\")' instead.")]
+        public void SetBasePath(String basePath)
+        {
+            // do nothing
+        }
+
+        /// <summary>
+        /// Gets or sets the configuration object
+        /// </summary>
+        /// <value>An instance of the Configuration</value>
+        public Configuration Configuration { get; set; }
+
+        /// <summary>
+        /// Gets the default header.
+        /// </summary>
+        /// <returns>Dictionary of HTTP header</returns>
+        [Obsolete("DefaultHeader is deprecated, please use Configuration.DefaultHeader instead.")]
+        public Dictionary<String, String> DefaultHeader()
+        {
+            return this.Configuration.DefaultHeader;
+        }
+
+        /// <summary>
+        /// Add default header.
+        /// </summary>
+        /// <param name="key">Header field name.</param>
+        /// <param name="value">Header field value.</param>
+        /// <returns></returns>
+        [Obsolete("AddDefaultHeader is deprecated, please use Configuration.AddDefaultHeader instead.")]
+        public void AddDefaultHeader(string key, string value)
+        {
+            this.Configuration.AddDefaultHeader(key, value);
+        }
+
+        /// <summary>
+        /// Creates a temporary "session" with parameters so that the user can be forwarded to the payment page with this context.
+        /// </summary>
+        /// <param name="payerFeeRequestModel">Contains the details from which to calculate the payer fees.</param>
+        /// <param name="impersonationAccountKey">The key that allows impersonation of another account for which the transaction is being processed. Only specify a value if the account being impersonated is different from the account that is submitting this request.</param>
+        public PayerFeeResponseModel PayerFeePost(PayerFeeRequestModel payerFeeRequestModel, string impersonationAccountKey)
+        {
+            // verify the required parameter 'postPaymentPageSessionRequestModel' is set
+            if (payerFeeRequestModel == null)
+                throw new ApiException(400, "Missing required parameter 'postPaymentPageSessionRequestModel' when calling PaymentPageSessionsApi->PaymentPageSessionsPost");
+
+            var localVarPath = "/api/v1/PayerFee";
+            var localVarPathParams = new Dictionary<String, String>();
+            var localVarQueryParams = new Dictionary<String, String>();
+            var localVarHeaderParams = new Dictionary<String, String>(Configuration.DefaultHeader);
+            var localVarFormParams = new Dictionary<String, String>();
+            var localVarFileParams = new Dictionary<String, FileParameter>();
+            Object localVarPostBody = null;
+
+            // to determine the Content-Type header
+            String[] localVarHttpContentTypes = new String[] {
+                "application/json", "text/json", "application/xml", "text/xml", "application/x-www-form-urlencoded"
+            };
+            String localVarHttpContentType = Configuration.ApiClient.SelectHeaderContentType(localVarHttpContentTypes);
+
+            // to determine the Accept header
+            String[] localVarHttpHeaderAccepts = new String[] {
+                "application/json", "text/json", "application/xml", "text/xml"
+            };
+            String localVarHttpHeaderAccept = Configuration.ApiClient.SelectHeaderAccept(localVarHttpHeaderAccepts);
+            if (localVarHttpHeaderAccept != null)
+                localVarHeaderParams.Add("Accept", localVarHttpHeaderAccept);
+
+            // set "format" to json by default
+            // e.g. /pet/{petId}.{format} becomes /pet/{petId}.json
+            localVarPathParams.Add("format", "json");
+
+            if (impersonationAccountKey != null) localVarHeaderParams.Add("impersonationAccountKey", Configuration.ApiClient.ParameterToString(impersonationAccountKey)); // header parameter
+
+            if (payerFeeRequestModel.GetType() != typeof(byte[]))
+            {
+                localVarPostBody = Configuration.ApiClient.Serialize(payerFeeRequestModel); // http body (model) parameter
+            }
+            else
+            {
+                localVarPostBody = payerFeeRequestModel; // byte array
+            }
+
+            // make the HTTP request
+            IRestResponse localVarResponse = (IRestResponse)Configuration.ApiClient.CallApi(localVarPath,
+                Method.POST, localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarFileParams,
+                localVarPathParams, localVarHttpContentType);
+
+            int localVarStatusCode = (int)localVarResponse.StatusCode;
+
+            if (localVarStatusCode >= 400)
+            {
+                var errorResponseModel = Newtonsoft.Json.JsonConvert.DeserializeObject<ErrorResponseModel>(localVarResponse.Content);
+
+                throw new ApiException(localVarStatusCode, errorResponseModel != null ? errorResponseModel.Message : null);
+            }
+            else if (localVarStatusCode == 0)
+                throw new ApiException(localVarStatusCode, localVarResponse.ErrorMessage, localVarResponse.ErrorMessage);
+
+            var responseContent = Newtonsoft.Json.JsonConvert.DeserializeObject<PayerFeeResponseModel>(localVarResponse.Content);
+
+            return responseContent;
+        }
+    }
+}
---
+++ epay3.Web.Api.Tests/PayerFeeFixture.cs
@@ -0,0 +1,43 @@
+﻿using epay3.Web.Api.Sdk.Api;
+using epay3.Web.Api.Sdk.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace epay3.Web.Api.Tests
+{
+    [TestClass]
+    public class When_Posting_An_Amount_To_Fee
+    {
+        private PayerFeeApi _payerFeeApi;
+        private TokensApi _tokensApi;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            _payerFeeApi = new PayerFeeApi(TestApiSettings.Uri);
+            _tokensApi = new TokensApi(TestApiSettings.Uri);
+
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(TestApiSettings.Key + ":" + TestApiSettings.Secret);
+
+            _payerFeeApi.Configuration.AddDefaultHeader("Authorization", "Basic " + System.Convert.ToBase64String(plainTextBytes));
+            _tokensApi.Configuration.AddDefaultHeader("Authorization", "Basic " + System.Convert.ToBase64String(plainTextBytes));
+        }
+
+        [TestMethod]
+        public void ShouldReturnValues()
+        {
+            var request = new PayerFeeRequestModel()
+            {
+                Amount = 5.3m
+            };
+
+            var response = _payerFeeApi.PayerFeePost(request, null);
+
+            Assert.IsInstanceOfType(response, typeof(PayerFeeResponseModel));
+            Assert.IsNotNull(response.AchFee);
+            Assert.IsNotNull(response.CreditCardFee);
+        }
+    }
+}
