952f42cd1fb39926339e954fecf9f9c2ff035489
Brennan McElhanon <brennan@epay3.com>
2018-09-10 10:58:49 -0500

    Added support for IvrSessions endpoint.

---
+++ epay3.Web.Api.Sdk/Model/PostIvrSessionResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epay3.Web.Api.Sdk.Model
+{
+    public class PostIvrSessionResponseModel
+    {
+        public bool Success { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
